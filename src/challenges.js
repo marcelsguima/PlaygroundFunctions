bc10adf8dfa2f6a1803c8c4516ee005e768caa27
marceslguima <marcelsguima@gmail.com>
2022-06-27 16:39:21 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  return frase.split(' ');
 }
 
 // Desafio 4
